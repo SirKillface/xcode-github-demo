a128c152cdb92e033e09dded786b4030cbacdd99
Paul Dalton <dalton.paul93@yahoo.com>
2022-05-18 11:50:50 -0400

    Initial Commit

---
+++ Shared/github_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_demoApp.swift
+//  Shared
+//
+//  Created by user215468 on 5/18/22.
+//
+
+import SwiftUI
+
+@main
+struct github_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
